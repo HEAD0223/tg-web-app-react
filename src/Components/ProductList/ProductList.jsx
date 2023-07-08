4d8c386a16c4317bdb1fbc3ccd0e6fa603d8a5ad
HEAD_0223 <andrei.dolgopol2000@gmail.com>
2023-07-08 21:54:38 +0300

    init

--- src/Components/ProductList/ProductList.jsx
+++ src/Components/ProductList/ProductList.jsx
@@ -30,7 +30,7 @@ export const ProductList = () => {
 			totalPrice: getTotalPrice(addedItems),
 			queryId,
 		};
-		fetch('http://localhost:8000', {
+		fetch('http://localhost:8000/web-data', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
