4ac70f1fdf109de22271057a93baf0ffc5c864d4
HEAD_0223 <andrei.dolgopol2000@gmail.com>
2023-07-08 19:07:35 +0300

    init

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import './Button.css';
 
 export const Button = (props) => {
-	return <button {...props} className={'button' + props.className} />;
+	return <button {...props} className={'button ' + props.className} />;
 };
