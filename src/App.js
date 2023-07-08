a9010630c157fc43f99be7f1492e427b52a4875c
HEAD_0223 <andrei.dolgopol2000@gmail.com>
2023-07-08 15:18:14 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import './App.css';
+import { Header } from './Components/Header/Header';
 import { useTelegram } from './Components/hooks/useTelegram';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
 
 	return (
 		<div className="App">
+			<Header />
 			<button onClick={onToggleButton}>Toggle</button>
 		</div>
 	);
